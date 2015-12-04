f04005f19ba72a338afde014ca7c119cade49037
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2015-12-04 18:37:12 +0100

    Fix merge issues

--- src/main/java/alien4cloud/paas/cloudify3/location/OpenstackLocationConfigurator.java
+++ src/main/java/alien4cloud/paas/cloudify3/location/OpenstackLocationConfigurator.java
@@ -22,7 +22,7 @@ public class OpenstackLocationConfigurator extends AbstractLocationConfigurator 
     @Inject
     private ResourceGenerator resourceGenerator;
 
-    public static final String COMPUTE_TYPE = "alien.nodes.openstack.LinuxCompute";
+    public static final String COMPUTE_TYPE = "alien.nodes.openstack.Compute";
     public static final String WINDOWS_COMPUTE_TYPE = "alien.nodes.openstack.WindowsCompute";
     public static final String IMAGE_TYPE = "alien.nodes.openstack.Image";
     public static final String WINDOWS_IMAGE_TYPE = "alien.nodes.openstack.WindowsImage";
